34d74ccfa34520870bb3056ce32002305e8db17b
madhuy1260 <madhu@rsaamerica.com>
2022-11-17 14:43:53 +0530

    gitpractice demo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/messenger/login" element={<Login />} />
+          <Route path="/messenger/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
